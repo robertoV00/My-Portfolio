61bf408b7317af5ec5c810e0fd0be9521662d231
Roberto Vega <droz.rv@gmail.com>
2025-01-17 14:03:27 -0500

    changed svg coloring, tweaked spinner animation, adjusted messageSent timer, added header text animations

--- app.js
+++ app.js
@@ -12,23 +12,36 @@ function closeMenu() {
     body.classList.remove('open')
 }
 
-// template_y2u8ck6
+// template_t1m3din
 // service_0woq3wf
 // NXu96JBx0Ud_x7fYO
 
 async function sendEmail(event) {
     //prevent the form from normally reloading
     event.preventDefault()
+
     const body = document.body
     const loading = document.querySelector('.contact-form-loading');
+    const form = document.querySelector('.contact-form')
 
-    //Loading state
-    loading.classList.remove("hidden")
-
-    //event.target returns all the values in the form
-    //this .sendForm() method connects to emailJS so that the email can be sent
-    await emailjs.sendForm("service_0woq3wf", "template_y2u8ck6", event.target, "NXu96JBx0Ud_x7fYO");
-
-    //Success state
-    console.log('The email has been sent. ')
+    try {
+        //Loading state
+        loading.classList.remove("hidden")
+    
+        //event.target returns all the values in the form
+        //this .sendForm() method connects to emailJS so that the email can be sent
+        await emailjs.sendForm("service_0woq3wf", "template_t1m3din", event.target, "NXu96JBx0Ud_x7fYO");        
+        
+        //Success state
+        form.reset() //resets all the input fields this way emails cannot be spammed
+        body.classList.add("success-open")
+        setTimeout(() => {
+            body.classList.remove("success-open")
+        }, 5000) //removes pop of message being sent
+        console.log('The email has been sent. ')
+        loading.classList.add("hidden")
+    } catch {
+        loading.classList.add("hidden")
+        alert ("An error has occured. Please try again later or contact me at robvega945@gmail.com...")
+    }
 }
